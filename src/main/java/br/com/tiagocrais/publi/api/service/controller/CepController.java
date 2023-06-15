a0a45c9b68d938005274309207eabb5ee8856c5b
Tiago Crais Camargo <tiagocrais@gmail.com>
2023-06-15 20:32:54 -0300

    Alterando path na controller.

--- src/main/java/br/com/tiagocrais/publi/api/service/controller/CepController.java
+++ src/main/java/br/com/tiagocrais/publi/api/service/controller/CepController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/consulta/cep")
+@RequestMapping("/api/consulta/cep")
 public class CepController {
 
     @Autowired
